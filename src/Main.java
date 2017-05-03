25da57ee2228a722a96837b6b16f76fef07e5932
Valentin Moullet <valentin@Valentins-MBP.fritz.box>
2017-05-03 12:13:08 +0200

    Modif main for nothing

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,11 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		System.out.println("----- Start parsing -----");
-		//new Parser(PATH_TO_PARSE, PARSED_PATH, FILE_TYPE).parse();
+		new Parser(PATH_TO_PARSE, PARSED_PATH, FILE_TYPE).parse();
 
 		
 		System.out.println("----- Start inserting -----");
-		new InsertCSV(PARSED_PATH);
+		//new InsertCSV(PARSED_PATH);
 		
 /*
 		System.out.println("Done");
