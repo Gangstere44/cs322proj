7282a61267c635ee75d04181dd45759859c47166
Gangstere44 <lavilleq@gmail.com>
2017-05-22 15:00:10 +0200

    setup base project

--- comics_server/comics/urls.py
+++ comics_server/comics/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+	url(r'^search$', views.search, name='search'),
+	url(r'^queries$', views.queries, name='queries'),
+	url(r'^.*$', RedirectView.as_view(pattern_name='search', permanent=False)),	
 ]
--- comics_server/comics/views.py
+++ comics_server/comics/views.py
@@ -6,5 +6,48 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-	return HttpResponse("Hello, world. You're at the comics index.")
+
+"""
+
+search:
+	
+	from python:
+
+		dico[table_name] = dico2
+
+		dico2[header_name] = array of header name
+		dico2[result] = array 
+							array of fields
+
+	from js:
+
+		word_to_find : String
+
+		tables : String with comma
+
+
+ queries:
+
+ 	from python:
+
+		dico2[header_name] = array of header name
+		dico2[result] = array 
+							array of fields
+
+ 	from js:
+
+ 		query_num : Int , 1 to n
+
+ 		parameters : String with comma
+
+"""
+
+def search(request):
+	context = {}
+
+	return render(request, 'search.html', context)
+
+def queries(request):
+	context = {}
+
+	return render(request, 'queries.html', context)
